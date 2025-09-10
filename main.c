2e636042f3dd0854fab7802ebdb94f81a6667440
sanae-sanae <ahjoubsanae19@gmail.com>
2025-09-10 15:03:35 +0200

    add algorithme <bubble sort> pui adding the menu of our project

--- main.c
+++ main.c
@@ -27,3 +27,63 @@ void afficherParPoste();
 void modifierJoueur();
 void supprimerJoueur();
 void rechercherJoueur();
+
+void bubbleSortNom(Joueur arr[], int n);
+void bubbleSortAge(Joueur arr[], int n);
+
+int main() {
+    int choix;
+    
+    printf("=== BIENVENUE DANS LA GESTION D'EQUIPE DE FOOTBALL ===\n\n");
+    
+    do {
+        afficherMenu();
+        printf("Votre choix: ");
+        scanf("%d", &choix);
+         switch(choix) {
+            case 1:
+                ajouterJoueur();
+                break;
+            case 2:
+                ajouterPlusieursJoueurs();
+                break;
+            case 3:
+                afficherTousJoueurs();
+                break;
+            case 4:
+                trierParNom();
+                break;
+            case 5:
+                trierParAge();
+                break;
+            case 6:
+                afficherParPoste();
+                break;
+            case 7:
+                modifierJoueur();
+                break;
+            case 8:
+                supprimerJoueur();
+                break;
+            case 9:
+                rechercherJoueur();
+                break;
+                break;
+            case 0:
+                printf("\nMerci d'avoir utilisé notre système de gestion!\n");
+                printf("Au revoir et bonne continuation!\n");
+                break;
+            default:
+                printf("\nChoix invalide! Veuillez réessayer.\n\n");
+        }
+        
+        if(choix != 0) {
+            printf("\nAppuyez sur Entrée pour continuer");
+            getchar();
+        }
+        
+    } while(choix != 0);
+    
+    return 0;
+}
+
